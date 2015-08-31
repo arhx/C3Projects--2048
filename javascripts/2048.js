57f8de9801f8a4fd21bcd70992f74de8248c3b8c
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-31 16:40:16 -0700

    got initial moveTile function working (always moving up); WIP

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -5,14 +5,12 @@ $(document).ready(function() {
     var direction = event.which;
     if(arrow_keys.indexOf(direction) > -1) {
       moveTiles(direction);
-      // moveTile(tile, event.which);
       event.preventDefault();
     }
   })
 })
 
 function moveTiles(direction) {
-  // find set of tiles that might move, loop through
   var farthestEdgeType = "";
   var farthestEdgeValue = "";
 
@@ -20,46 +18,52 @@ function moveTiles(direction) {
     case 38: // up
       farthestEdgeType = "data-row";
       farthestEdgeValue = "r0";
+      break;
     case 40: // down
       farthestEdgeType = "data-row";
       farthestEdgeValue = "r3";
+      break;
     case 37: // left
       farthestEdgeType = "data-col";
       farthestEdgeValue = "c0";
+      break;
     case 39: // right
       farthestEdgeType = "data-col";
       farthestEdgeValue = "c3";
+      break;
   }
 
-  var allTiles = $('.tile');
-  var movableTiles = [];
-
-  for (var i = 0, i < allTiles.length, i++) {
-    if (allTiles[i].attr(farthestEdgeType) != farthestEdgeValue ) {
-      movableTiles.push(allTiles[i]);
-    }
+  var tiles = $(".tile");
+  for (i = 0; i < tiles.length; i++) {
+    moveTile(tiles[i], direction);
   }
-
 
 }
 
 function moveTile(tile, direction) {
-  var new_tile_value = tile.attr("data-val") * 2;
-  tile.attr("data-val", new_tile_value);
-  tile.text(new_tile_value);
-
-  switch(direction) {
-    case 38: //up
-      tile.attr("data-row","r0");
-      break;
-    case 40: //down
-      tile.attr("data-row","r3");
-      break;
-    case 37: //left
-      tile.attr("data-col","c0");
-      break;
-    case 39: //right
-      tile.attr("data-col","c3");
-      break;
-  }
+  console.log(tile);
+  console.log(tile.getAttribute("data-row"));
+  // move UP
+  tile.setAttribute("data-row", "r0");
 }
+
+// function moveTile(tile, direction) {
+//   var new_tile_value = tile.attr("data-val") * 2;
+//   tile.attr("data-val", new_tile_value);
+//   tile.text(new_tile_value);
+
+//   switch(direction) {
+//     case 38: //up
+//       tile.attr("data-row","r0");
+//       break;
+//     case 40: //down
+//       tile.attr("data-row","r3");
+//       break;
+//     case 37: //left
+//       tile.attr("data-col","c0");
+//       break;
+//     case 39: //right
+//       tile.attr("data-col","c3");
+//       break;
+//   }
+// }
