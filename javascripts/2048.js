e0bfb1ebcc0ae65f5e79961549483209e80cf922
Shanna Chau <shannachau@gmail.com>
2015-08-31 15:29:15 -0700

    Started implementation for moveTiles function.

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -2,13 +2,46 @@ $(document).ready(function() {
   console.log('ready!');
   $('body').keydown(function(event){
     var arrow_keys = [37, 38, 39, 40];
-    if(arrow_keys.indexOf(event.which) > -1) {
-      var tile = $('.tile');
-      moveTile(tile, event.which);
+    var direction = event.which;
+    if(arrow_keys.indexOf(direction) > -1) {
+      moveTiles(direction);
+      // moveTile(tile, event.which);
       event.preventDefault();
     }
   })
 })
+
+function moveTiles(direction) {
+  // find set of tiles that might move, loop through
+  var farthestEdgeType = "";
+  var farthestEdgeValue = "";
+
+  switch(direction) {
+    case 38: // up
+      farthestEdgeType = "data-row";
+      farthestEdgeValue = "r0";
+    case 40: // down
+      farthestEdgeType = "data-row";
+      farthestEdgeValue = "r3";
+    case 37: // left
+      farthestEdgeType = "data-col";
+      farthestEdgeValue = "c0";
+    case 39: // right
+      farthestEdgeType = "data-col";
+      farthestEdgeValue = "c3";
+  }
+
+  var allTiles = $('.tile');
+  var movableTiles = [];
+
+  for (var i = 0, i < allTiles.length, i++) {
+    if (allTiles[i].attr(farthestEdgeType) != farthestEdgeValue ) {
+      movableTiles.push(allTiles[i]);
+    }
+  }
+
+
+}
 
 function moveTile(tile, direction) {
   var new_tile_value = tile.attr("data-val") * 2;
