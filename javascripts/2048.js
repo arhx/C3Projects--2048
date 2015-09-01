1872e376b1379d59dba5923b499cd18d3926b984
Shanna Chau <shannachau@gmail.com>
2015-08-31 17:17:39 -0700

    Trying to revert to previous commit

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -5,19 +5,14 @@ $(document).ready(function() {
     var direction = event.which;
     if(arrow_keys.indexOf(direction) > -1) {
       moveTiles(direction);
+      // moveTile(tile, event.which);
       event.preventDefault();
     }
   })
 })
 
 function moveTiles(direction) {
-  var tiles = $(".tile");
-  for (i = 0; i < tiles.length; i++) {
-    moveTile(tiles[i], direction);
-  }
-}
-
-function moveTile(tile, direction) {
+  // find set of tiles that might move, loop through
   var farthestEdgeType = "";
   var farthestEdgeValue = "";
 
@@ -25,20 +20,46 @@ function moveTile(tile, direction) {
     case 38: // up
       farthestEdgeType = "data-row";
       farthestEdgeValue = "r0";
-      break;
     case 40: // down
       farthestEdgeType = "data-row";
       farthestEdgeValue = "r3";
-      break;
     case 37: // left
       farthestEdgeType = "data-col";
       farthestEdgeValue = "c0";
-      break;
     case 39: // right
       farthestEdgeType = "data-col";
       farthestEdgeValue = "c3";
-      break;
   }
 
-  tile.setAttribute(farthestEdgeType, farthestEdgeValue);
+  var allTiles = $('.tile');
+  var movableTiles = [];
+
+  for (var i = 0; i < allTiles.length; i++) {
+    if (allTiles[i].attr(farthestEdgeType) != farthestEdgeValue ) {
+      movableTiles.push(allTiles[i]);
+    }
+  }
+
+
+}
+
+function moveTile(tile, direction) {
+  var new_tile_value = tile.attr("data-val") * 2;
+  tile.attr("data-val", new_tile_value);
+  tile.text(new_tile_value);
+
+  switch(direction) {
+    case 38: //up
+      tile.attr("data-row","1");
+      break;
+    case 40: //down
+      tile.attr("data-row","4");
+      break;
+    case 37: //left
+      tile.attr("data-col","1");
+      break;
+    case 39: //right
+      tile.attr("data-col","4");
+      break;
+  }
 }
